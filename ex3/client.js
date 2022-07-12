b346cd12b67cc76a4063e3b7ab0a80fdea7bbb9c
Erik de Gouveia Zambom <egz@ug2c10.windows.cin.ufpe.br>
2022-07-12 18:38:21 -0300

    Improving code

--- ex3/client.js
+++ ex3/client.js
@@ -8,6 +8,11 @@ const rl = readline.createInterface({
 
 const client = dgram.createSocket('udp4');
 
+client.on('error', error => {
+    console.log(`Error: ${error}`);
+    client.close();
+});
+
 client.on('message', (msg, info) => {
     console.log(`\x1b[32mServer: ${msg}\x1b[0m`);
 });
@@ -16,5 +21,11 @@ rl.addListener('line', line => {
     readline.moveCursor(process.stdout, 0, -1);
     readline.clearScreenDown(process.stdout);
     
+    console.log(`\x1b[34mMe: ${line.toString()}\x1b[0m`);
+    
     client.send(line.toString(), 8081, '127.0.0.1');
+
+    if (line.toString().toLowerCase() === 'bye') {
+        client.close();
+    }
 });
--- ex3/server.js
+++ ex3/server.js
@@ -1,4 +1,10 @@
 const dgram = require('dgram');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const socket = dgram.createSocket('udp4');
 
@@ -8,7 +14,20 @@ socket.on('error', error => {
 });
 
 socket.on('message', (msg, rinfo) => {
-    console.log(`message: ${msg} | info: ${rinfo.address} - ${rinfo.port}`);
+    console.log(`\x1b[32mClient: ${msg}\x1b[0m`);
+
+    rl.addListener('line', line => {
+        readline.moveCursor(process.stdout, 0, -1);
+        readline.clearScreenDown(process.stdout);
+
+        console.log(`\x1b[34mMe: ${line.toString()}\x1b[0m`);
+        
+        socket.send(line.toString(), rinfo.port, rinfo.address);
+        
+        if (line.toString().toLowerCase() === 'bye') {
+            socket.close();
+        }
+    });
 });
 
 socket.bind(8081);
